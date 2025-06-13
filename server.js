0a53eff4fd7a6d1b7ecc3a59d03b2183f3d571f7
Leandro Fernandes <lfernandes1898@gmail.com>
2025-06-13 14:37:43 -0300

    Criação da applicação com as funcionalidades de adicionar anotações, listar anotações, sistema de login usando autenticação JWT e a realização da conexão com MongoDB

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+require('dotenv').config();
+dotenv.config();
+
+console.log('Mongo URI:', process.env.MONGO_URI);  // <-- Aqui, para conferir a variável
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Importando rotas
+const userRoutes = require('./routes/userRoutes');
+const noteRoutes = require('./routes/noteRoutes');
+
+// Conexão do MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected!'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/notes', noteRoutes);
+
+// Iniciando Servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server rodando em: http://localhost:${PORT}`));
