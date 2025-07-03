acd0de40424488078071ce5cb182faa4601d13fd
debora-evilaine <sousxdebora@gmail.com>
2025-07-03 18:59:42 -0300

    Ajuste na renderização do markdown

--- front-end/insercao_de_notas/scripts/utils.js
+++ front-end/insercao_de_notas/scripts/utils.js
@@ -1,12 +1,5 @@
 export const renderMarkdown = (text) => {
-    return text
-        .replace(/^### (.*$)/gim, "<h3>$1</h3>")
-        .replace(/^## (.*$)/gim, "<h2>$1</h2>")
-        .replace(/^# (.*$)/gim, "<h1>$1</h1>")
-        .replace(/\*\*(.*?)\*\*/gim, "<strong>$1</strong>")
-        .replace(/\*(.*?)\*/gim, "<em>$1</em>")
-        .replace(/`(.*?)`/gim, "<code>$1</code>")
-        .replace(/\n/gim, "<br>");
+    return window.marked.parse(text);
 };
 
 export const escapeHtml = (text) => {
