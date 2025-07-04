dd83310ca838d7dc2083a990b02730cbd7cda67e
Leandro Fernandes <lfernandes1898@gmail.com>
2025-07-04 18:05:18 -0300

    Criando o gerador de pdf das notas selecionadas pelo usuário

--- api/src/routes/noteRoutes.js
+++ api/src/routes/noteRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Note = require('../models/Notes');
+const PDFDocument = require('pdfkit');
 const authenticate = require('../middleware/authenticate');
 
 // converter para UTC-3
@@ -167,5 +168,57 @@ router.get('/by-date', authenticate, async (req, res) => {
     });
   }
 });
+
+
+
+
+//gerar pdf das notas selecionas
+
+router.post('/download-notes',  authenticate, async (req, res) => {
+  try {
+    // Espera um array de IDs no corpo da requisição
+    const { noteIds } = req.body; 
+    if (!Array.isArray(noteIds) || noteIds.length === 0) {
+      return res.status(400).json({ error: 'Nenhuma nota selecionada' });
+    }
+
+    // Busca as notas no banco de dados
+    const notes = await Note.find({ _id: { $in: noteIds }, user: req.userId });
+
+    if (notes.length === 0) {
+      return res.status(404).json({ error: 'Notas não encontradas' });
+    }
+
+    // Cria um novo documento PDF
+    const doc = new PDFDocument();
+
+    // Define o header para download de PDF no response
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=notas.pdf');
+
+    // Pipe do PDF para o response
+    doc.pipe(res);
+
+    // Escreve o conteúdo de cada nota no PDF
+    notes.forEach((note, index) => {
+      doc.moveDown(0.5);
+      doc.fontSize(12).text(`${note.title || 'Sem título'}`);
+      doc.moveDown(0.2);
+      doc.fontSize(10).text(note.content || 'Sem conteúdo');
+     if (index < notes.length - 1) {
+    doc.addPage();
+  }
+    });
+
+    // Finaliza o documento PDF
+    doc.end();
+
+  } catch (error) {
+    console.error('Erro ao gerar PDF:', error);
+    res.status(500).json({ error: 'Erro ao gerar PDF' });
+  }
+});
+
+
 
 module.exports = router;
